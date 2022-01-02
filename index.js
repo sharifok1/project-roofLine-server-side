a31633b47f15e67e4c3d0fb0f808d1e4031c9f62
sharifok1 <sharifok1@gmail.com>
2022-01-03 01:38:32 +0600

    add users

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const { MongoClient } = require("mongodb");
 const cors = require('cors');
+const app = express();
 require('dotenv').config();
-const { MongoClient } = require('mongodb');
-const port = process.env.PORT || 5000;
-
+// const  ObjectId = require('mongodb').ObjectId;
+const port = process.env.PORT || 5000
+app.use(cors());
+app.use(express.json());
 
 app.use(cors());
 
@@ -14,18 +16,88 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('database connected successfully');
+        const database = client.db('RoofLine');
+        const users = database.collection('userCollection');
+        // const OurServices = database.collection('OurServices');
+        // const myUserCollection = database.collection('users');
+      
+     //Post method  Method (single document)-----------API--Post
+     app.post('/userCollection', async(req,res)=>{
+      const user = req.body;
+      const result = await users.insertOne(user);
+      res.json(result)
+      console.log(result)
+    })
+    app.put('/userCollection', async(req, res)=>{
+        const user =req.body;
+        const filter = {email:user.email} ;
+        const options = {upsert: true};
+        const updateDoc = {$set: user};
+        const result  = await users.updateOne(filter,updateDoc,options);
+        res.json(result)
+        console.log(result);
+      })
+    //  Get Metod  get all RiderCollection---------------------API--get all
+     app.get('/userCollection', async(req, res)=>{
+      const user = users.find({});
+      const result = await user.toArray();
+      res.send(result)
+    })
+      //
+      //  //update method for make admin-----------------------admin api
+       app.put('/users/admin', async(req, res)=>{
+        const user = req.body;
+        // console.log(user)
+        const filter = {email: user.email};
+        const updateDoc = {$set:{role:'admin'}};
+        const result = await users.updateOne(filter, updateDoc);
+        res.json(result);
+      })
+      //get admin user-----------------------------------get admin user
+      app.get('/users/:email', async(req, res) =>{
+        const email = req.params.email;
+        const query = { email: email};
+        const user = await users.findOne(query)
+        let isAdmin = false;
+        if(user?.role == 'admin'){
+          isAdmin = true
+        }
+        res.json({admin: isAdmin})
+      })
+    
+      
+    
+     
+    // Get method for find specefic document by id---------API-- get one
+    // app.get('/myDocs/:id', async(req,res)=>{
+    //   const id = req.params.id;
+    //   const quary = {_id:ObjectId(id)};
+    //   const result = await myDocsCollection.findOne(quary);
+    //   res.send(result)
+    
+    // })
+      //Delete Method  delete a doc-----------------API--Delete 
+      // app.delete('/myDocs/:id', async(req, res)=>{
+      //   const id = req.params.id;
+      //   const query = {_id:ObjectId(id)};
+      //   const result = await myDocsCollection.deleteOne(query);
+      //   // console.log('deleted id', result);
+      //   res.json(result); 
+      // })
+    
+    
+        
+      } finally {
+        // await client.close();
+      }
     }
-    finally {
-        //await client.close();
-    }
-}
-run().catch(console.dir);
-
-app.get('/', (req, res) => {
-    res.send('Team32')
-})
-
-app.listen(port, () => {
-    console.log(`listening at ${port}`)
-})
+    run().catch(console.dir);
+    
+    
+    app.get('/' , (req, res)=>{
+      res.json('team32 working on......');
+    })
+    
+    app.listen(port, ()=> {
+      console.log('team32 listing from',port)
+    })
